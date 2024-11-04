439c966f616c3e606d6ab89a9510d9a3723c839c
Daniel <dan@gmail.com>
2024-11-04 19:15:31 +0000

    test org

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,22 +6,24 @@ from unittest.mock import patch, MagicMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ """
+    """Test class for TestGithubOrgClient"""
     @parameterized.expand([
         ('google'),
         ('abc')
         ])
     @patch('client.get_json')
     def test_org(self, parameter, mock_get_json):
-        """ Test org() if it returns a correct value """
-        """ Test get_json is called once with the expected arg """
-        """ make sure get_json is not executed """
+        """
+           Test org() if it returns a correct value
+           Test get_json is called once with the expected arg
+           make sure get_json is not executed
+        """
 
         url = "https://api.github.com/orgs/{}".format(parameter)
 
-        # This method should test that GithubOrgClient.org returns the correct value.
+        # This method should test GithubOrgClient.org()
         my_object = GithubOrgClient(parameter)
         my_object.org()
 
-        # Use @patch as a decorator to make sure get_json is called once with the expected argument but make sure it is not executed.
+        # Use @patch as a decorator to make sure get_json is called once
         mock_get_json.assert_called_once_with(url)
