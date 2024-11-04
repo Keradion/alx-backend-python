c90887ba9bfeba6948954bf496db4a15c33904fb
Daniel <dan@gmail.com>
2024-11-04 15:45:25 +0000

    utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -91,24 +91,3 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
 
-class TestGithubOrgClient(unittest.TestCase):
-    """ """
-    @parameterized.expand([
-        ('google'), 
-        ('abc')
-        ])
-    @patch('client.get_json')
-    def test_org(self, parameter, mock_get_json):
-        """ Test org() if it returns a correct value """
-        """ Test get_json is called once with the expected arg """
-        """ make sure get_json is not executed """
-        
-        url = "https://api.github.com/orgs/{}".format(parameter)
-        
-        # This method should test that GithubOrgClient.org returns the correct value.
-        my_object = GithubOrgClient(parameter)
-        my_object.org()
-        
-        # Use @patch as a decorator to make sure get_json is called once with the expected argument but make sure it is not executed.
-        mock_get_json.assert_called_once_with(url)
-
