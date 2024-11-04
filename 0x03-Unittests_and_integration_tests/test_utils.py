82f386b4fbba764db3d8bf4a128120cf5ec48ff7
Daniel <dan@gmail.com>
2024-11-04 08:42:31 +0000

    mock http requests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,8 @@
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
-
+from unittest.mock import patch, MagicMock
+from utils import get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -30,3 +31,33 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(arg1, arg2)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+       The test class inherits from unittest and consist
+       Multiple Test for get_json() from utils class.
+    """
+    @patch('utils.requests')
+    def test_get_json(self, mock_request):
+        """Test get_json method of utils http calls."""
+        test_url = "http://example.com"
+        test_payload={"payload": True}
+        
+        # Create a mock for http response object
+        mock_response = MagicMock()
+
+        # Set status_code for http response object
+        mock_response.status_code = 200
+
+        # Set return value for Json() of http response object
+        mock_response.json.return_value = test_payload
+
+        mock_request.get.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+
+        test_url="http://holberton.io"
+        test_payload={"payload": False}
+        mock_response.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+
